3a6614398efd0c6699465d11c0284e088dd10c0b
Nicoleta Anghel <nicoletaanghel1998@gmail.com>
2024-11-11 21:39:25 +0200

    Update CheckOutPage.py

--- Esitetest_automated_tests/Esitetest/source/pages/CheckOutPage.py
+++ Esitetest_automated_tests/Esitetest/source/pages/CheckOutPage.py
@@ -8,7 +8,7 @@ import logging as logger
 
 class CheckOutPage(CheckOutPAgeLocator):
 
-    endpoint = '/?page_id=9'
+    endpoint = '/checkout/'
     def __init__(self, driver):
         self.driver = driver
         self.sl = SeleniumExtended(self.driver)
